2415a254a9c80f6c48b571acdf37be4404c6c3f6
Jonathan Bruckman <96599491+Makomanix@users.noreply.github.com>
2023-12-25 00:45:14 -0500

    completed opacity toggle feather

--- etch.js
+++ etch.js
@@ -6,6 +6,8 @@ let column;
 
 let rainbow = false;
 
+let opacity = false;
+
 let selectedColor = false;
 
 let colorPicker = document.querySelector('.color');
@@ -16,6 +18,9 @@ let boxColor = colorPicker.value;
 buttons.forEach((button) => {
   button.addEventListener('click', (e) => {
     switch(e.target.name) {
+      case "opacity":
+        console.log(e.target.name)
+        toggleOpacity();
       case "reset":
         console.log(e.target.name)
         reset();
@@ -37,6 +42,10 @@ buttons.forEach((button) => {
   })
 });
 
+function toggleOpacity() {
+  boxArray = [];
+  return opacity ? opacity = false : opacity = true;
+};
 
 function reset() {
   eraseGrid();
@@ -64,7 +73,7 @@ function selectGridSize(){
 
   let size = prompt("Select Grid Size between 10 and 100", "");
 
-  if (size < 10 || size > 100 || size === "") {
+  if (size < 10 || size > 50 || size === "") {
     alert("Grid size entered was not between 10 and 100. Try again!")
     size = prompt("Select Grid Size between 10 and 100", "")
   }
@@ -83,6 +92,8 @@ function toggleRainbow(){
   return rainbow ? rainbow = false : rainbow = true;
 };
 
+let boxArray = [];
+
 
 // Create the grid
 function createGrid(size = 16){
@@ -94,8 +105,10 @@ function createGrid(size = 16){
         grid.appendChild(column);
         for (let j = 0; j < size; j++) {
           let box = document.createElement('div');
+          let boxObject = {count: 0};
+          boxArray.push(boxObject)
           box.classList.toggle('box');
-          box.innerText = 'hi';
+          box.innerText = boxArray.length;
           box.style.width = 40/size + "rem";
           box.style.height = 40/size + "rem";
           box.style.backgroundColor = '#ffffff';
@@ -103,19 +116,31 @@ function createGrid(size = 16){
           column.appendChild(box);
         }
     }
+    console.log(boxArray);
 };
+
 
 function fillBlock(e) {
   console.log(boxColor)
 
   boxColor = colorPicker.value;
+  console.log(boxColor)
 
   if (rainbow === true) {
     boxColor = '#' + Math.random().toString(16).substring(2, 8)
   };
 
+  if ( opacity === true) {
+    let count = boxArray[e.target.innerText].count += 1
+    e.target.style.backgroundColor = boxColor;
+    if ( boxArray[e.target.innerText].count >= 10 ) {
+      e.target.style.opacity = 1.0;
+    } else {
+      e.target.style.opacity = count * .1;
+    };
+  };
+  
   e.target.style.backgroundColor = boxColor;
-  console.log(e.target.style.backgroundColor);
 };
 
 

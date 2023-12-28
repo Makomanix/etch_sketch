e2bff1fbcc512ba6be30c384b3c4e1d4e867e499
Jonathan Bruckman <96599491+Makomanix@users.noreply.github.com>
2023-12-27 22:50:34 -0500

    corrected how opacity functionality works and added CSS to create etch a sketch appearance

--- etch.js
+++ etch.js
@@ -2,50 +2,51 @@ const grid = document.querySelector('.grid');
 
 const buttons = document.querySelectorAll('button');
 
-let column;
+let colorPicker = document.querySelector('.color');
+colorPicker.addEventListener('click', selectColor)
 
 let rainbow = false;
 
 let opacity = false;
 
 let selectedColor = false;
+
 let size;
 
-let colorPicker = document.querySelector('.color');
-colorPicker.addEventListener('click', selectColor)
-
 let boxColor = colorPicker.value;
+
+let boxArray = [];
 
 buttons.forEach((button) => {
   button.addEventListener('click', (e) => {
     switch(e.target.name) {
+      
       case "opacity":
-        console.log(e.target.name)
         toggleOpacity();
+        break;
+
       case "reset":
-        console.log(e.target.name)
         reset();
         break;
+
       case "grid size":
-        console.log(e.target.name);
         selectGridSize(e);
         break;
+
       case "toggle":
-        console.log(e.target.name);
         toggleRainbow(e);
-        console.log(rainbow)
         break;
+
       case "select":
-        console.log(e.target.name);
         selectColor(e);
         break;
+
     }
   })
 });
 
-function toggleOpacity() {
-  boxArray = [];
-  return opacity ? opacity = false : opacity = true;
+function toggleOpacity() { 
+  opacity ? opacity = false : opacity = true;
 };
 
 function reset() {
@@ -62,7 +63,6 @@ function reset() {
 function eraseGrid() {
   let erase = document.querySelectorAll('.column')
   let eraseArray = Array.from(erase);
-  console.log(erase);
 
   if (eraseArray.length > 0) {
     eraseArray.forEach((erased) => {grid.removeChild(erased)}) 
@@ -74,20 +74,20 @@ function selectGridSize(){
   boxArray = [];
   eraseGrid();
 
-  size = prompt("Select Grid Size between 10 and 100", "");
+  size = prompt("Select Grid Size between 10 and 50", "");
 
   if (size < 10 || size > 50 || size === "") {
-    alert("Grid size entered was not between 10 and 100. Try again!")
-    size = prompt("Select Grid Size between 10 and 100", "")
+
+    alert("Grid size entered was not between 10 and 50. Try again!")
+    size = prompt("Select Grid Size between 10 and 50", "")
+
   }
-  console.log(size)
+
   createGrid(size)
 };
 
 function selectColor(e){
-  console.log(colorPicker.value);
   rainbow = false;
-  console.log(rainbow)
 };
 
 function toggleRainbow(){
@@ -95,39 +95,41 @@ function toggleRainbow(){
   return rainbow ? rainbow = false : rainbow = true;
 };
 
-let boxArray = [];
-
-
 // Create the grid
 function createGrid(size = 16){
 
     for (let i = 0; i < size; i++) {
-      console.log('make columns')
-        column = document.createElement('div');
+
+        let column = document.createElement('div');
         column.classList.toggle('column');
         grid.appendChild(column);
+
         for (let j = 0; j < size; j++) {
+
           let box = document.createElement('div');
           let boxObject = {count: 0};
-          boxArray.push(boxObject)
-          box.classList.toggle('box');
+          boxArray.push(boxObject);
+
           box.innerText = boxArray.length;
-          box.style.width = 40/size + "rem";
+
+          box.classList.toggle('box');
           box.style.height = 40/size + "rem";
+          box.style.width = 40/size + "rem";
           box.style.backgroundColor = '#ffffff';
+          
+
           box.addEventListener('mouseover', fillBlock);
+
           column.appendChild(box);
         }
     }
-    console.log(boxArray);
 };
 
 
 function fillBlock(e) {
-  console.log(boxColor)
-
+  
   boxColor = colorPicker.value;
-  console.log(boxColor)
+  e.target.style.opacity = '1';
 
   if (rainbow === true) {
     boxColor = '#' + Math.random().toString(16).substring(2, 8)
@@ -139,13 +141,14 @@ function fillBlock(e) {
     
     if ( boxArray[(e.target.innerText) - 1].count >= 10 ) {
       e.target.style.opacity = 1.0;
+
     } else {
       e.target.style.opacity = count * .1;
-    };
+    }
   };
   
   e.target.style.backgroundColor = boxColor;
-};
 
+};
 
 createGrid();
